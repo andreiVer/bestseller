1ccecaacc74c49869b1d6c924ca8ef7d4fb373be
andrei <andreiverdes@yahoo.com>
2026-08-16 16:26:22 +0200

    adds err to product composable

--- app/composables/products.ts
+++ app/composables/products.ts
@@ -12,16 +12,21 @@ export function useProducts() {
     abortController = new AbortController();
 
     isLoadingProducts.value = true;
+
     try {
       const response = await $fetch<Product[]>('/api/products', {
         method: 'GET',
         signal: abortController.signal
       });
+
       console.log(response);
       products.value = response;
       hasLoadedProducts.value = true;
-    } catch (err) {
-      console.log(err);
+    } catch {
+      throw createError({
+        statusCode: 404,
+        statusMessage: 'Could not fetch products'
+      });
     } finally {
       isLoadingProducts.value = false;
     }
