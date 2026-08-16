3e90e750aea7b92ad7f4aef70953dd8f21b6f7fb
andrei <andreiverdes@yahoo.com>
2026-08-16 03:15:55 +0200

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,28 @@
+import perfectionist from 'eslint-plugin-perfectionist';
+
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-  // Your custom configs here
-)
+  {
+    plugins: {
+      perfectionist
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+      'perfectionist/sort-imports': ['error', {
+        order: 'asc',
+        type: 'alphabetical'
+      }],
+      'perfectionist/sort-interfaces': ['error'],
+      'perfectionist/sort-objects': ['error', {
+        order: 'asc',
+        type: 'alphabetical'
+      }],
+      'perfectionist/sort-variable-declarations': ['error', {
+        order: 'asc',
+        type: 'alphabetical'
+      }]
+    }
+  }
+);
