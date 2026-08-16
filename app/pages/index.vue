faeed783cba12e17b26c6428620ee18224417def
andrei <andreiverdes@yahoo.com>
2026-08-16 03:21:38 +0200

    makes index product page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,21 @@
+<script lang="ts" setup>
+const { getProducts, products } = useProducts();
+
+getProducts();
+</script>
+
 <template>
-  <div>index works</div>
+  <UPageGrid
+    class="
+    grid-cols-2 gap-4
+    lg:grid-cols-3
+    xl:grid-cols-4
+  "
+  >
+    <ProductCard
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </UPageGrid>
 </template>
